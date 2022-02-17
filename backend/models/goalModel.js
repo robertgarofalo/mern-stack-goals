95af3cf8ee18e467e94c8ebb452d4fc242382eb7
Robert <r.s.garofalo87@gmail.com>
2022-02-18 07:16:39 +1100

    add goal routes and goal controller

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 
-const goalSchema = mongoose.Schema({
+const goalSchema = mongoose.Schema(
+    {
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User', // as per module.exports in the userModel.js file
+    },
     text: {
         type: String,
         required: [true, 'Please add a textvalue']
